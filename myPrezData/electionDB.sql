f9b4afe405cf020d5096094aa40934162ad05d53
Jota Sprout <jotasprout@gmail.com>
2017-11-18 10:50:59 -0500

    More Queries and Data

--- myPrezData/electionDB.sql
+++ myPrezData/electionDB.sql
@@ -1,17 +1,5 @@
 CREATE DATABASE prezplaypro;
 
-CREATE TABLE results (
-  stateAbbr varchar(2) NOT NULL,
-  candidateID int(2) NOT NULL,
-  popVotes int(7) NOT NULL,
-  year int(4) NOT NULL,
-  FOREIGN KEY (candidateID) REFERENCES candidates (candidateID),
-  FOREIGN KEY (stateAbbr) REFERENCES states (stateAbbr),
-  INDEX (stateAbbr),
-  INDEX (candidateID),
-  INDEX (year)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 CREATE TABLE candidates (
   candidateID int(2) NOT NULL,
   candidateName varchar(72) NOT NULL,
@@ -22,6 +10,20 @@ CREATE TABLE parties (
   partyAbbr varchar(3) NOT NULL,
   partyName varchar(72) NOT NULL,
   PRIMARY KEY (partyAbbr),
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE states (
+  stateAbbr varchar(2) NOT NULL,
+  stateName varchar(72) NOT NULL,
+  PRIMARY KEY (stateAbbr),
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE electoralCollege (
+  stateAbbr varchar(2) NOT NULL,
+  year1992 int(2) NOT NULL,
+  year2004 int(2) NOT NULL,
+  year2012 int(2) NOT NULL,
+  FOREIGN KEY (stateAbbr) REFERENCES states (stateAbbr)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE affiliations (
@@ -35,18 +37,14 @@ CREATE TABLE affiliations (
   INDEX (year)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE states (
+CREATE TABLE results (
   stateAbbr varchar(2) NOT NULL,
-  stateName varchar(72) NOT NULL,
-  PRIMARY KEY (stateAbbr),
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE electoralCollege (
-  stateAbbr varchar(2) NOT NULL,
-  1992 int(2) NOT NULL,
-  2004 int(2) NOT NULL,
-  2012 int(2) NOT NULL,
-  INDEX (1992),
-  INDEX (2004),
-  INDEX (2012)
+  candidateID int(2) NOT NULL,
+  popVotes int(7) NOT NULL,
+  year int(4) NOT NULL,
+  FOREIGN KEY (candidateID) REFERENCES candidates (candidateID),
+  FOREIGN KEY (stateAbbr) REFERENCES states (stateAbbr),
+  INDEX (stateAbbr),
+  INDEX (candidateID),
+  INDEX (year)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
