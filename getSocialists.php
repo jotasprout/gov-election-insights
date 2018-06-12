71be8485e882926ebb349073b81855eeade546dc
Jay Sprout <jotasprout@gmail.com>
2018-06-12 12:08:49 -0400

    national all socialist results query

--- getSocialists.php
+++ getSocialists.php
@@ -14,4 +14,16 @@ JOIN (SELECT a.candidateID, a.partyAbbr, p.partyName, c.candidateName
             ON a.partyAbbr = p.partyAbbr) s 
     ON s.candidateID = f.candidateID
 ORDER BY f.popVotes";
+
+$getSocialistResultsFL = "SELECT f.candidateID, f.stateAbbr, f.popVotes, s.partyAbbr, s.partyName, s.candidateName
+FROM results2016 f
+JOIN (SELECT a.candidateID, a.partyAbbr, p.partyName, c.candidateName 
+        FROM affiliations2016 a 
+        JOIN candidates2016 c 
+            ON a.candidateID = c.candidateID 
+        JOIN (SELECT * FROM partiesspectrum WHERE rating = 1) p 
+            ON a.partyAbbr = p.partyAbbr) s 
+    ON s.candidateID = f.candidateID
+ORDER BY f.stateAbbr";
+
 ?>
