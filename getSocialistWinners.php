8485b6f42fb23504dfaf8c83a3406d1385121477
JotaSprout <jotasprout@gmail.com>
2018-06-17 17:07:05 -0400

    Separate Colored Socialists

--- getSocialistWinners.php
+++ getSocialistWinners.php
@@ -23,7 +23,7 @@ JOIN candidates2016 z
 ON z.candidateID = y.candidateID
 ORDER BY y.stateAbbr";
 
-$getSocialistWinners = "SELECT m.stateAbbr, m.candidateName, max(m.popVotes) AS mostVotes FROM
+$getSocialistWinners = "SELECT m.stateAbbr, m.candidateID, m.candidateName, max(m.popVotes) AS mostVotes FROM
 (SELECT y.*, z.candidateName
 FROM (SELECT r.stateAbbr, r.popVotes, r.candidateID
     FROM results2016 r
@@ -32,13 +32,32 @@ FROM (SELECT r.stateAbbr, r.popVotes, r.candidateID
         FROM affiliations2016 a 
         JOIN (SELECT * FROM partiesspectrum WHERE rating = 1) p 
         ON a.partyAbbr = p.partyAbbr)
-    AND r.candidateID != 3) y
+    AND r.candidateID != 3
+    AND r.popVotes > 0) y
 JOIN candidates2016 z
 ON z.candidateID = y.candidateID
 ORDER BY y.stateAbbr) m
 GROUP BY m.stateAbbr";
 
-$result = mysqli_query($connekt, $getSocialistWinners);
+$getSocialistWinners2 = "SELECT m.stateAbbr, m.stateName, m.candidateID, m.candidateName, max(m.popVotes) AS mostVotes FROM
+(SELECT y.*, z.candidateName, k.stateName
+FROM (SELECT r.stateAbbr, r.popVotes, r.candidateID
+    FROM results2016 r
+    WHERE r.candidateID IN
+        (SELECT a.candidateID 
+        FROM affiliations2016 a 
+        JOIN (SELECT * FROM partiesspectrum WHERE rating = 1) p 
+        ON a.partyAbbr = p.partyAbbr)
+    AND r.candidateID != 3
+    AND r.popVotes > 0) y
+JOIN candidates2016 z
+ON z.candidateID = y.candidateID
+JOIN states k
+ON k.stateAbbr = y.stateAbbr
+ORDER BY y.stateAbbr) m
+GROUP BY m.stateAbbr";
+
+$result = mysqli_query($connekt, $getSocialistWinners2);
 
 if (!$result){
     echo "Nope. I got nothin.";
