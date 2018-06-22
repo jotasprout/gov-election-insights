0edc1169031f23209a2d794c2171cd63990ad80b
JotaSprout <jotasprout@gmail.com>
2018-06-22 14:47:27 -0400

    IDB can read and write

--- localdb.js
+++ localdb.js
@@ -3,3 +3,19 @@ const dbPromise = idb.open('testdb', 1, upgradeDB => {
     keyValStore.put ('douche', 'trump');
 });
 
+dbPromise.then (db => {
+    const tx = db.transaction ('testdb');
+    const testStore = tx.objectStore('testdb');
+    return testStore.get('trump');
+}).then (val => {
+    console.log ('The value of "trump" is: ', val);
+});
+
+dbPromise.then (db => {
+    const tx = db.transaction('testdb', 'readwrite');
+    const testStore = tx.objectStore('testdb');
+    testStore.put('douche-lite', 'clinton');
+    return tx.complete;
+}).then (function(){
+    console.log('Added clinton:douche-lite to testdb');
+});
