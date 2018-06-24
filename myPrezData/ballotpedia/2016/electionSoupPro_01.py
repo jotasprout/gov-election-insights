78f08ede7c6786ddad169ccfa281cd25c85e43d6
JotaSprout <jotasprout@gmail.com>
2018-06-24 18:24:40 -0400

    So far nothing works

--- myPrezData/ballotpedia/2016/electionSoupPro_01.py
+++ myPrezData/ballotpedia/2016/electionSoupPro_01.py
@@ -4,13 +4,14 @@ from bs4 import BeautifulSoup
 
 # Grab local file I downloaded
 # Can I use a variable in the next line and loop through an array for all 51 files?
-htmlDoc = open("Florida_2016.htm")
-soup = BeautifulSoup(htmlDoc)
+htmlDoc = open("Florida_2016.html")
+soup = BeautifulSoup(htmlDoc, 'html.parser')
+
 allResults = []
 
 # Grab just the results table
 
 
 # create JSON file in which to put leftover soup
-with open("results.json", "w") as outfile:
-    json.dump(allResults, outfile)
+# with open("results.json", "w") as outfile:
+#    json.dump(allResults, outfile)
