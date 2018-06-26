99c8a548bcd6eeeecfb47cbf87d064d1aa77ab9c
Jay Sprout <jotasprout@gmail.com>
2018-06-26 16:35:52 -0400

    first successful ballotpedia scraper test

--- myPrezData/ballotpedia/2016/electionSoupPro_01.py
+++ myPrezData/ballotpedia/2016/electionSoupPro_01.py
@@ -4,11 +4,16 @@ from bs4 import BeautifulSoup
 
 # Grab local file I downloaded
 # Can I use a variable in the next line and loop through an array for all 51 files?
-htmlDoc = open("Florida_2016.html")
-soup = BeautifulSoup(htmlDoc, 'html.parser')
+html = open("Florida_2016.html")
+soup = BeautifulSoup(html, 'html.parser')
 
 allResults = []
 
+headspan = soup.find(id="Results")
+justhead = headspan.parent
+resultstable = justhead.find_next("table")
+hrow = resultstable.select("tr:nth-of-type(2)")
+print (hrow)
 # Grab just the results table
 
 
