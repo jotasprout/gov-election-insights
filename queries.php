1162d02a1031b4c0d2604f13af01f4c44c4edd48
JotaSprout <jotasprout@gmail.com>
2018-10-07 16:10:37 -0400

    Purging old queries and code

--- queries.php
+++ queries.php
@@ -1,61 +1,5 @@
 <?php
-
-$artistInfoRecentWithArt = "SELECT a.artistID AS artistID, a.artistArt AS artistArt, a.artistName AS artistName, p1.pop AS pop, p1.date AS date
-    FROM artists a
-    JOIN (SELECT p.*
-			FROM popArtists p
-			INNER JOIN (SELECT artistID, pop, max(date) AS MaxDate
-						FROM popArtists  
-						GROUP BY artistID) groupedp
-			ON p.artistID = groupedp.artistID
-			AND p.date = groupedp.MaxDate) p1
-	ON a.artistID = p1.artistID
-	ORDER BY p1.pop DESC";
-
-$happyScabies2 = "SELECT a.albumName, a.year, a.albumArt, z.artistName, p1.pop, p1.date, a.albumID
-	FROM (SELECT
-				y.albumID AS albumID,
-				y.albumName AS albumName,
-				y.artistID AS artistID,
-				y.albumArt AS albumArt,
-				y.year AS year
-			FROM albums y 
-			WHERE y.artistID = '$artistID') a
-	JOIN artists z ON z.artistID = '$artistID'
-	JOIN (SELECT p.*
-			FROM popAlbums p
-			INNER JOIN (SELECT albumID, pop, max(date) AS MaxDate
-						FROM popAlbums  
-						GROUP BY albumID) groupedp
-			ON p.albumID = groupedp.albumID
-			AND p.date = groupedp.MaxDate) p1 
-	ON a.albumID = p1.albumID
-	ORDER BY year ASC;";
-
-$winners = "SELECT a.*
-			FROM results2016 a
-			INNER JOIN (SELECT candidateID, stateAbbr, max(popVotes) AS maxVotes
-						FROM results2016  
-						GROUP BY stateAbbr) groupStateVotes
-			ON a.popVotes = groupStateVotes.maxVotes
-			ORDER BY stateAbbr";
-
-
-$getSocialistWinners = "SELECT y.*, z.candidateName
-						FROM (SELECT r.stateAbbr, r.popVotes, r.candidateID
-							FROM results2016 r
-							WHERE r.candidateID IN
-								(SELECT a.candidateID 
-								FROM affiliations2016 a 
-								JOIN (SELECT * FROM partiesspectrum WHERE rating = 1) p 
-								ON a.partyAbbr = p.partyAbbr)
-							AND r.candidateID != 3
-							AND r.popVotes > 0) y
-						JOIN candidates2016 z
-						ON z.candidateID = y.candidateID
-						ORDER BY y.stateAbbr";
-
-						
+					
 $getSocialistResults = "SELECT y.*, z.candidateName
 						FROM (SELECT r.stateAbbr, r.popVotes, r.candidateID
 							FROM results2016 r
@@ -103,9 +47,5 @@ $newTable = "CREATE TABLE partiesspectrum (partyAbbr varchar(3) NOT NULL, PRIMAR
 $dropKick="ALTER TABLE affiliations2016 DROP FOREIGN KEY affiliations2016_ibfk_2";
 
 $newKick="ALTER TABLE affiliations2016 ADD CONSTRAINT partyAbbr_ibfk FOREIGN KEY(partyAbbr) REFERENCES partiesspectrum (partyAbbr) ON UPDATE CASCADE ON DELETE CASCADE";
-
-
-
-
 
 ?>
