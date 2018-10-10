cb3ed4a272e5f61e33d8db80a886e543bae116cf
jotasprout <jotasprout@gmail.com>
2018-10-10 14:06:50 -0400

    example function

--- getSocialistWinners.php
+++ getSocialistWinners.php
@@ -8,6 +8,26 @@ if (!$connekt) {
     echo 'Shit! Did not connect.';
 };
 
+$happyScabies2 = "SELECT a.albumName, a.year, a.albumArt, z.artistName, p1.pop, p1.date, a.albumID
+	FROM (SELECT
+				y.albumID AS albumID,
+				y.albumName AS albumName,
+				y.artistID AS artistID,
+				y.albumArt AS albumArt,
+				y.year AS year
+			FROM albums y 
+			WHERE y.artistID = '$artistID') a
+	JOIN artists z ON z.artistID = '$artistID'
+	JOIN (SELECT p.*
+			FROM popAlbums p
+			INNER JOIN (SELECT albumID, pop, max(date) AS MaxDate
+						FROM popAlbums  
+						GROUP BY albumID) groupedp
+			ON p.albumID = groupedp.albumID
+			AND p.date = groupedp.MaxDate) p1 
+	ON a.albumID = p1.albumID
+	ORDER BY year ASC;";
+
 $getSocialistWinners = "SELECT m.stateAbbr, m.stateName, m.candidateID, m.candidateName, max(m.popVotes) AS mostVotes FROM
 (SELECT y.*, z.candidateName, k.stateName
 FROM (SELECT r.stateAbbr, r.popVotes, r.candidateID
