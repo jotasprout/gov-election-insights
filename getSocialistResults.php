536a651d46e214f9dfa7e340c2aa0dc60c1b9aa9
jotasprout <jotasprout@gmail.com>
2018-10-18 16:06:00 -0400

    making progress

--- getSocialistResults.php
+++ getSocialistResults.php
@@ -23,6 +23,20 @@ ON z.candidateID = y.candidateID
 JOIN states s ON s.stateAbbr = y.stateAbbr
 ORDER BY y.stateAbbr";
 
+$getSocialistResultsWithZero = "SELECT y.*, z.candidateName, s.stateName
+FROM (SELECT r.stateAbbr, r.popVotes, r.candidateID
+    FROM results2016 r
+    WHERE r.candidateID IN
+        (SELECT a.candidateID 
+        FROM affiliations2016 a 
+        JOIN (SELECT * FROM partiesspectrum WHERE rating = 1) p 
+        ON a.partyAbbr = p.partyAbbr)
+    AND r.candidateID != 3) y
+JOIN candidates2016 z
+ON z.candidateID = y.candidateID
+JOIN states s ON s.stateAbbr = y.stateAbbr
+ORDER BY y.stateAbbr";
+
 $result = mysqli_query($connekt, $getSocialistResults);
 
 if (!$result){
