c5d08996913de3109a17f48a8f280280ac1b12cc
JotaSprout <jotasprout@gmail.com>
2018-11-07 21:52:28 -0500

    all left and trump i think

--- getAllLeftAndTrump.php
+++ getAllLeftAndTrump.php
@@ -24,6 +24,11 @@ ON z.candidateID = y.candidateID
 JOIN states s ON s.stateAbbr = y.stateAbbr
 ORDER BY y.stateAbbr";
 
+$allLeftAndTrump = "SELECT * FROM results2016 r
+WHERE (r.candidateID IN  (3,1,12,13,14,17,18,20,22,27,28,30,31)
+OR r.candidateID = 29)
+AND WHERE r.popVotes > 0";
+
 $result = mysqli_query($connekt, $getSocialistResultsWithZero);
 
 if (!$result){
