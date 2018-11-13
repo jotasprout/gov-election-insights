409a4d8427fc2556e79af2bb0cfa6c9157d83ebe
jotasprout <jotasprout@gmail.com>
2018-11-13 11:10:43 -0500

    allResults works but partyName breaks it

--- everyone.php
+++ everyone.php
@@ -8,17 +8,19 @@ if (!$connekt) {
     echo 'Shit! Did not connect.';
 };
 
-$everyone = "SELECT y.stateAbbr, z.stateName, y.candidateID, z.candidateName, z.partyAbbr, z.partyName, y.popVotes
-FROM (SELECT r.stateAbbr AS stateAbbr, r.popVotes AS popVotes, r.candidateID AS candidateID
-    FROM results2016 r
-    WHERE r.popVotes > 0) y
-    JOIN (SELECT a.stateAbbr, a.candidateID, c.candidateName, a.partyAbbr, p.partyName, s.stateName
-        FROM affiliations2016 a
-        JOIN partiesspectrum p ON a.partyAbbr = p.partyAbbr
-        JOIN candidates2016 c ON c.candidateID = a.candidateID
-        JOIN states s ON s.stateAbbr = a.stateAbbr) z 
-    ON z.stateAbbr = y.stateAbbr AND z.candidateID = y.candidateID
-ORDER BY y.stateAbbr";
+$everyone = "SELECT y.*, c.candidateName, s.stateName
+                FROM (SELECT r.* FROM results2016 r WHERE r.popVotes > 0) y
+                JOIN affiliations2016 a ON y.candidateID = a.candidateID AND y.stateAbbr = a.stateAbbr
+                JOIN candidates2016 c ON c.candidateID = y.candidateID
+                JOIN states s ON s.stateAbbr = y.stateAbbr
+            ORDER BY y.stateAbbr";
+
+$everyone2 = "SELECT y.*, c.candidateName, s.stateName, a.partyName
+                FROM (SELECT r.* FROM results2016 r WHERE r.popVotes > 0) y
+                JOIN affiliations2016 a ON y.candidateID = a.candidateID AND y.stateAbbr = a.stateAbbr
+                JOIN candidates2016 c ON c.candidateID = y.candidateID
+                JOIN states s ON s.stateAbbr = y.stateAbbr
+            ORDER BY y.stateAbbr";            
 
 $result = mysqli_query($connekt, $everyone);
 
