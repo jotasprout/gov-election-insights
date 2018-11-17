ff853efba58961233761dc8dc7d2a32143a4b25d
JotaSprout <jotasprout@gmail.com>
2018-11-17 17:26:09 -0500

    combiny

--- spectrum.js
+++ spectrum.js
@@ -6,6 +6,6 @@ let allRightNoTrumpNorJohnson = [2,4,7,8,9,15,25];
 
 let allLeft = [3,1,12,13,14,17,18,20,22,27,28,30,31];
 
-let noGreen = [];
+let noGreen = [12,13,17,18,20,22,27,30,31];
 
-let allSocialists = [];
+let allSocialists = [12,13,17,18,20,22,27,28,30,31];
