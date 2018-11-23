ea5deb846d93d84eb8f72ed3ed8586d4a570b4e1
jotasprout <jotasprout@gmail.com>
2018-11-23 14:07:22 -0500

    added McMullin to allRight arrays

--- spectrum.js
+++ spectrum.js
@@ -1,23 +1,26 @@
-let allRight = [2,4,7,8,9,11,15,25,29];
+let allRight = [2,4,7,8,9,11,15,21,25,29];
 
-let clinton = [3];
+let allRightNoTrump = [2,4,7,8,9,11,15,21,25];
 
-let trump = [29];
-
-let allRightAndClinton = [2,3,4,7,8,9,11,15,25,29];
-
-let allRightNoTrump = [2,4,7,8,9,11,15,25];
-
-let allRightNoTrumpNorJohnson = [2,4,7,8,9,15,25];
+let allRightNoTrumpNorJohnson = [2,4,7,8,9,15,21,25];
 
 let allLeft = [3,1,12,13,14,17,18,20,22,27,28,30,31];
-
-let allLeftAndRight = [3,1,12,13,14,17,18,20,22,27,28,30,31,2,4,7,8,9,11,15,25,29];
-
-let allLeftAndTrump = [3,1,12,13,14,17,18,20,22,27,28,29,30,31];
 
 let noGreen = [12,13,17,18,20,22,27,30,31];
 
 let allSocialists = [12,13,17,18,20,22,27,28,30,31];
 
+let clinton = [3];
+
+let trump = [29];
+
 let everyone = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+
+// I can delete the rest, correct?
+
+let allRightAndClinton = [2,3,4,7,8,9,11,15,25,29];
+
+let allLeftAndRight = [3,1,12,13,14,17,18,20,22,27,28,30,31,2,4,7,8,9,11,15,25,29];
+
+let allLeftAndTrump = [3,1,12,13,14,17,18,20,22,27,28,29,30,31];
+
